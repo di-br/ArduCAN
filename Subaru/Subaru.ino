09370168a9446238bb0433933f90d494c455527a
di-br <di-br@users.noreply.github.com>
2018-02-13 21:29:31 +0100

    Moved SparkFun's MCP related files to enable changes. Added a first test on CAN messages: blinken lights via break and clutch pedal.

--- Subaru/Subaru.ino
+++ Subaru/Subaru.ino
@@ -13,11 +13,10 @@
  ****************************************************************************/
 
 //********************************headers************************************//
-#include <Canbus.h>
-#include <defaults.h>
-#include <global.h>
-#include <mcp2515.h>
-#include <mcp2515_defs.h>
+#include "defaults.h"
+#include "MCP2515.h"
+#include "MCP2515_defs.h"
+#include "global.h"
 #include <SD.h>
 
 //********************************declarations*******************************//
@@ -27,6 +26,16 @@ File dataFile;
 
 // initialize uSD pins
 const int chipSelect = 9;
+
+// LEDs on the CAN shield
+const int LED1 = 7; // green  PORTD 7
+const int LED2 = 8; // green  PORTB 0
+#define LED1S D,7   // and a short-cut for speedier changes
+#define LED2S B,0
+
+/* this does not blink when uSD in use?
+ // LED on the RedBoard
+ const int LED3 = 13; // blue */
 
 // switch between serial and uSD I/O for the main loop
 #define _USD_IO
@@ -42,30 +51,31 @@ const int chipSelect = 9;
 void setup() {
   // for debug use
   Serial.begin(115200);
-  Serial.println("CAN Read - Testing receival of CAN Bus message");
-  delay(250);
+  delay(100);
 
   // initialise MCP2515 CAN controller at the specified speed
-  if(Canbus.init(CANSPEED_500))
-    Serial.println("CAN Init ok");
+  if(mcp2515_init(CANSPEED_500))
+    Serial.println("CAN Init: ok");
   else
-    Serial.println("Can't init CAN");
-  delay(250);
+    Serial.println("CAN Init: failed");
+  delay(500);
 
+#ifdef _USD_IO
   // check if uSD card initialised
   if (!SD.begin(chipSelect)) {
-    Serial.println("uSD card failed to initialize, or is not present");
+    Serial.println("uSD card: failed to initialise or not present");
     return;
   }
   else{
-    Serial.println("uSD card initialized.");
+    Serial.println("uSD card: initialised");
     delay(50);
   }   
-  delay(250);
+  delay(500);
 
   // open uSD file to start logging data, this will append to the file
-  File dataFile = SD.open("data.txt", FILE_WRITE);
-  if (dataFile){
+  File dataFile = SD.open("can.log", FILE_WRITE);
+  if (dataFile)
+  {
     // log that we did initialise to separate different 'sessions'
     dataFile.print("Init OK");
     dataFile.println();
@@ -73,6 +83,11 @@ void setup() {
     dataFile.flush();
     dataFile.close();
   }
+#endif
+
+  // finish by setting up LEDs
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
 }
 
 //********************************main loop*********************************//
@@ -86,42 +101,66 @@ void loop(){
 #ifdef _USD_IO
   // open uSD file to log data
   File dataFile = SD.open("can.log", FILE_WRITE);
+  if(dataFile)
+  {
 #endif
 
-  if (mcp2515_check_message())
-  {
-    if (mcp2515_get_message(&message))
+    if (mcp2515_check_message())
     {
-      timeStamp = millis()*0.001 + 1000000000;
+      if (mcp2515_get_message(&message))
+      {
+        timeStamp = millis();
 
-      // output received data to either Serial or uSD
-      // mimic the candump format here for easier replay
-      // timestamp and 'interface'
-      IO_U.print("(");
-      IO_U.print(timeStamp);
-      IO_U.print(") arcan0 ");
-      // CAN id
-      if (message.id<0x100) IO_U.print("0");
-      if (message.id<0x10)  IO_U.print("0");
-      IO_U.print(message.id,HEX);
-      IO_U.print("#");
-      // CAN payload
-      IO_U.print(message.header.length,DEC);
-      for(int i=0;i<message.header.length;i++) 
-      {	
-        if (message.data[i]<0x10) IO_U.print("0");
-        IO_U.print(message.data[i],HEX);
+        // output received data to either Serial or uSD
+        // mimic the candump format here for easier replay
+        // timestamp and 'interface'
+        IO_U.print("(");
+        IO_U.print(timeStamp);
+        IO_U.print(") arcan0 ");
+        // CAN id
+        if (message.id < 0x100) IO_U.print("0");
+        if (message.id < 0x10)  IO_U.print("0");
+        IO_U.print(message.id,HEX);
+        IO_U.print("#");
+        // CAN payload
+        IO_U.print(message.header.length,DEC);
+        for(int i=0;i<message.header.length;i++)
+        {
+          if (message.data[i] < 0x10) IO_U.print("0");
+          IO_U.print(message.data[i],HEX);
+          IO_U.print(" ");
+        }
+        // CRLF and flush
+        IO_U.println("");
+        IO_U.flush();
       }
-      // CRLF and flush
-      IO_U.println("");
-      IO_U.flush();
+      // inspect CAN frame 0x411
+      if (message.id == 0x411)
+      {
+        // check for break switch
+        if (message.data[6] & 0x1<<4) {
+          SET(LED1S);
+          //IO_U.println("BREAK");
+        }
+        else RESET(LED1S);
+      }
+      // inspect CAN frame 0x600
+      if (message.id == 0x600)
+      {
+        // check for clutch switch
+        if (message.data[6] & 0x1<<2) {
+          SET(LED2S);
+          //IO_U.println("CLUTCH");
+        }
+        else RESET(LED2S);
+      }
     }
-  }
 
 #ifdef _USD_IO
+  }
   // flush and close file
   dataFile.flush();
   dataFile.close();
 #endif
-
 }
+
---
+++ Subaru/defaults.h
@@ -0,0 +1,18 @@
+#ifndef	DEFAULTS_H
+#define	DEFAULTS_H
+
+#define	P_MOSI          B,3
+#define	P_MISO          B,4
+#define	P_SCK           B,5
+
+//#define MCP2515_CS      D,3 // Rev A
+#define MCP2515_CS      B,2 // Rev B
+#define MCP2515_INT     D,2
+#define LED2_HIGH       B,0
+#define LED2_LOW        B,0
+
+#define CANSPEED_125    7               // CAN speed at 125 kbps
+#define CANSPEED_250    3               // CAN speed at 250 kbps
+#define CANSPEED_500    1               // CAN speed at 500 kbps
+
+#endif	// DEFAULTS_H
