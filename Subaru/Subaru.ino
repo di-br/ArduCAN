77bd41dffcde0fe070b1e7da5ef041154e0aebb6
di-br <di-br@users.noreply.github.com>
2018-02-16 22:33:45 +0100

    First attempt to actively talk to the ECU, quering DPF status. Only kind of works, timing issues.

--- Subaru/Subaru.ino
+++ Subaru/Subaru.ino
@@ -32,7 +32,7 @@
 
 // flash LEDs when DPF regen is active
 //     not implemented (yet?)
-//#define _DPF
+#define _DPF
 
 //********************************declarations*******************************//
 
@@ -76,7 +76,8 @@ void setup() {
 
 #ifdef _USD_IO
   // check if uSD card initialised
-  if (!SD.begin(chipSelect)) {
+  if (!SD.begin(chipSelect))
+  {
     Serial.println("uSD card: failed to initialise or not present");
     return;
   }
@@ -97,7 +98,7 @@ void setup() {
     dataFile.flush();
     dataFile.close();
   }
-#endif
+#endif // _USD_IO
 
   // finish by setting up LEDs
   pinMode(LED1, OUTPUT);
@@ -117,7 +118,7 @@ void loop(){
   File dataFile = SD.open("can.log", FILE_WRITE);
   if(dataFile)
   {
-#endif
+#endif // _USD_IO
 
     if (mcp2515_check_message())
     {
@@ -138,7 +139,6 @@ void loop(){
         IO_U.print(message.id,HEX);
         IO_U.print("#");
         // CAN payload
-        IO_U.print(message.header.length,DEC);
         for(int i=0;i<message.header.length;i++)
         {
           if (message.data[i] < 0x10) IO_U.print("0");
@@ -148,14 +148,15 @@ void loop(){
         // CRLF and flush
         IO_U.println("");
         IO_U.flush();
-#endif
+#endif // _LOG
 
 #ifdef _LBC
         // inspect CAN frame 0x411
         if (message.id == 0x411)
         {
           // check for break switch
-          if (message.data[6] & 0x1<<4) {
+          if (message.data[6] & 0x1<<4)
+          {
             SET(LED1S);
             //IO_U.println("BREAK");
           }
@@ -165,37 +166,258 @@ void loop(){
         if (message.id == 0x600)
         {
           // check for clutch switch
-          if (message.data[6] & 0x1<<2) {
+          if (message.data[6] & 0x1<<2)
+          {
             SET(LED2S);
             //IO_U.println("CLUTCH");
           }
           else RESET(LED2S);
         }
-#endif
-
-#ifdef _DPF
-        // Hmm... initially I thought DPF regen would be announced in a CAN frame somewhere.
-        // This is not the case. So we have to query the ECU if we want to indicate this.
-        // To add some fun to this: we need ISO 15765-4 for this...
-
-        // DPF regen count: addresses 0x00029E + 0x00029D
-        // send 0x7e0 : 10 08 A8 00 00 02 9E 00
-        // receive 0x7e8 : 03 00 00 00 00 00 00 00
-        // send 0x7e0 : 21 02 9D 00 00 00 00 00
-        // receive 0x7e8 ...
-
-        // DPF regen active: address 0x0001CE
-        // send 0x7e0 : 05 A8 00 00 01 CE 00 00
-        // reveive 0x7e8 ...
-#endif
+#endif // _LBC
       }
-
     }
 
+#ifdef _DPF
+    // Hmm... initially I thought DPF regen would be announced in a CAN frame somewhere.
+    // This is not the case. So we have to query the ECU if we want to indicate this.
+    // To add some fun to this: we need ISO 15765-4 for this...
+
+    // DPF regen count: addresses 0x00029E + 0x00029D
+    // send 0x7e0 : 10 08 A8 00 00 02 9E 00
+    // receive 0x7e8 : 30 00 00 00 00 00 00 00
+    //    if we see something like 31 ... or 03 ... we are asked to wait or abort
+    // send 0x7e0 : 21 02 9D 00 00 00 00 00
+    // receive 0x7e8 ...
+    IO_U.println("Trying to receive DPF regen count");
+    // the single frame way, rather clumsy
+    tCAN tx_message;
+
+    tx_message.id = 0x7e0;
+    tx_message.header.rtr = 0;
+    tx_message.header.length = 8;
+    tx_message.data[0] = 0x05;
+    tx_message.data[1] = 0xA8;
+    tx_message.data[2] = 0x00;
+    tx_message.data[3] = 0x00;
+    tx_message.data[4] = 0x02;
+    tx_message.data[5] = 0x9E;
+    tx_message.data[6] = 0x00;
+    tx_message.data[7] = 0x00;
+
+    SET(LED1S);
+    delay(200);
+    RESET(LED1S);
+    // we will wait for the reply for 250ms
+    unsigned long timeout;
+    timeout = millis();
+
+    mcp2515_bit_modify(CANCTRL, (1<<REQOP2)|(1<<REQOP1)|(1<<REQOP0), 0);
+    if (mcp2515_send_message(&tx_message))
+    {
+      IO_U.println("1st request sent");
+    }
+    else IO_U.println("Some error...");
+
+    // reveive 0x7e8 ...
+    // loop until we have what we want or timeout is reached
+    bool MSG_RCV = false;
+    while(!MSG_RCV)
+    {
+      if (mcp2515_check_message())
+      {
+        if (mcp2515_get_message(&message))
+        {
+          if (message.id == 0x7e8)
+          {
+            IO_U.println("1st answer received:");
+            SET(LED2S);
+            delay(200);
+            RESET(LED2S);
+            // log message
+            // CAN id
+            IO_U.print(message.id,HEX);
+            IO_U.print("#");
+            // CAN payload
+            for(int i=0;i<message.header.length;i++)
+            {
+              if (message.data[i] < 0x10) IO_U.print("0");
+              IO_U.print(message.data[i],HEX);
+              IO_U.print(" ");
+            }
+            // CRLF and flush
+            IO_U.println("");
+            IO_U.flush();
+            MSG_RCV = true;
+          }
+          else MSG_RCV = false;
+        }
+      }
+      if (millis() > timeout + 250)
+      {
+        MSG_RCV = true;
+        SET(LED1S);
+        SET(LED2S);
+        delay(200);
+        RESET(LED1S);
+        RESET(LED2S);
+        IO_U.println("1st reply timed out");
+      }
+    }
+    tx_message.id = 0x7e0;
+    tx_message.header.rtr = 0;
+    tx_message.header.length = 8;
+    tx_message.data[0] = 0x05;
+    tx_message.data[1] = 0xA8;
+    tx_message.data[2] = 0x00;
+    tx_message.data[3] = 0x00;
+    tx_message.data[4] = 0x02;
+    tx_message.data[5] = 0x9D;
+    tx_message.data[6] = 0x00;
+    tx_message.data[7] = 0x00;
+
+    SET(LED1S);
+    delay(200);
+    RESET(LED1S);
+    // we will wait for the reply for 250ms
+    timeout = millis();
+
+    mcp2515_bit_modify(CANCTRL, (1<<REQOP2)|(1<<REQOP1)|(1<<REQOP0), 0);
+    if (mcp2515_send_message(&tx_message))
+    {
+      IO_U.println("2nd request sent");
+    }
+    else IO_U.println("Some error...");
+
+    // reveive 0x7e8 ...
+    // loop until we have what we want or timeout is reached
+    MSG_RCV = false;
+    while(!MSG_RCV)
+    {
+      if (mcp2515_check_message())
+      {
+        if (mcp2515_get_message(&message))
+        {
+          if (message.id == 0x7e8)
+          {
+            IO_U.println("2nd answer received:");
+            SET(LED2S);
+            delay(200);
+            RESET(LED2S);
+            // log message
+            // CAN id
+            IO_U.print(message.id,HEX);
+            IO_U.print("#");
+            // CAN payload
+            for(int i=0;i<message.header.length;i++)
+            {
+              if (message.data[i] < 0x10) IO_U.print("0");
+              IO_U.print(message.data[i],HEX);
+              IO_U.print(" ");
+            }
+            // CRLF and flush
+            IO_U.println("");
+            IO_U.flush();
+            MSG_RCV = true;
+          }
+          else MSG_RCV = false;
+        }
+      }
+      if (millis() > timeout + 250)
+      {
+        MSG_RCV = true;
+        IO_U.println("2nd reply timed out");
+        SET(LED1S);
+        SET(LED2S);
+        delay(200);
+        RESET(LED1S);
+        RESET(LED2S);
+      }
+    }
+
+    IO_U.println("Trying to check active DPF regen");
+    // DPF regen active: address 0x0001CE
+    // send 0x7e0 : 05 A8 00 00 01 CE 00 00
+//    tCAN tx_message;
+
+    tx_message.id = 0x7e0;
+    tx_message.header.rtr = 0;
+    tx_message.header.length = 8;
+    tx_message.data[0] = 0x05;
+    tx_message.data[1] = 0xA8;
+    tx_message.data[2] = 0x00;
+    tx_message.data[3] = 0x00;
+    tx_message.data[4] = 0x01;
+    tx_message.data[5] = 0xCE;
+    tx_message.data[6] = 0x00;
+    tx_message.data[7] = 0x00;
+
+    SET(LED1S);
+    delay(200);
+    RESET(LED1S);
+    // we will wait for the reply for 250ms
+//    unsigned long timeout;
+    timeout = millis();
+
+    mcp2515_bit_modify(CANCTRL, (1<<REQOP2)|(1<<REQOP1)|(1<<REQOP0), 0);
+    if (mcp2515_send_message(&tx_message))
+    {
+      IO_U.println("Request sent");
+    }
+    else IO_U.println("Some error...");
+
+    // reveive 0x7e8 ...
+    // loop until we have what we want or timeout is reached
+//    bool MSG_RCV = false;
+    MSG_RCV = false;
+    while(!MSG_RCV)
+    {
+      if (mcp2515_check_message())
+      {
+        if (mcp2515_get_message(&message))
+        {
+          if (message.id == 0x7e8)
+          {
+            IO_U.println("Answer received:");
+            SET(LED2S);
+            delay(200);
+            RESET(LED2S);
+            // log message
+            // CAN id
+            IO_U.print(message.id,HEX);
+            IO_U.print("#");
+            // CAN payload
+            for(int i=0;i<message.header.length;i++)
+            {
+              if (message.data[i] < 0x10) IO_U.print("0");
+              IO_U.print(message.data[i],HEX);
+              IO_U.print(" ");
+            }
+            // CRLF and flush
+            IO_U.println("");
+            IO_U.flush();
+            MSG_RCV = true;
+          }
+          else MSG_RCV = false;
+        }
+      }
+      if (millis() > timeout + 250)
+      {
+        MSG_RCV = true;
+        IO_U.println("Reply timed out");
+        SET(LED1S);
+        SET(LED2S);
+        delay(200);
+        RESET(LED1S);
+        RESET(LED2S);
+      }
+    }
+    delay(5000);
+#endif // _DPF
+
 #ifdef _USD_IO
-  }
+  } // if(dataFile)
   // flush and close file
   dataFile.flush();
   dataFile.close();
-#endif
+#endif // _USD_IO
 }
